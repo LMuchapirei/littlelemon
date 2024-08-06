c26e9a64b18beeb401e1baddad0d728e8de1aa5c
LMuchapirei <muchapireil@gmail.com>
2024-08-06 09:22:36 +0200

    Initial commit

---
+++ littlelemon/littlelemon/littlelemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  littlelemonApp.swift
+//  littlelemon
+//
+//  Created by Linval Muchapirei on 6/8/2024.
+//
+
+import SwiftUI
+
+@main
+struct littlelemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
