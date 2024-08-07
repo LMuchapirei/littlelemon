5c42824966d14c02bc0da0d70ce37d851a87d35b
LMuchapirei <muchapireil@gmail.com>
2024-08-07 08:25:17 +0200

    Add fields needed for the onboarding screen

--- littlelemon/littlelemon/Onboarding.swift
+++ littlelemon/littlelemon/Onboarding.swift
@@ -7,9 +7,35 @@
 
 import SwiftUI
 
+let kFirstName = "first name key"
+let kLastName = "last name key"
+let kEmail = "email key"
+
 struct Onboarding: View {
+    @State private var firstName: String = ""
+    @State private var lastName: String = ""
+    @State private var email: String = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            
+            TextField("First Name",text:$firstName)
+            TextField("Last Name",text:$lastName)
+            TextField("Email",text:$email)
+            
+            Button("Register")
+                    {
+                        print("Register Hit")
+                        if !firstName.isEmpty && !lastName.isEmpty && !email.isEmpty {
+                            UserDefaults.standard.set(firstName, forKey: kFirstName)
+                            UserDefaults.standard.set(lastName, forKey: kLastName)
+                            UserDefaults.standard.set(email, forKey: kEmail)
+                        }
+                    }
+            .padding(.horizontal,20)
+             .padding(.vertical,10)
+             .background(.green,in:.rect(cornerRadius: 10))
+             .foregroundColor(.white)
+        }
     }
 }
 
