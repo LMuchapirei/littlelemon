e17c105dc57c7fd24943b19aae12989e37d8b0c6
LMuchapirei <muchapireil@gmail.com>
2024-08-07 08:54:59 +0200

    Add the user profile page

--- littlelemon/littlelemon/UserProfile.swift
+++ littlelemon/littlelemon/UserProfile.swift
@@ -8,8 +8,21 @@
 import SwiftUI
 
 struct UserProfile: View {
+    @Environment(\.presentationMode) var presentation
+    let firstName: String  = UserDefaults.standard.string(forKey: kFirstName) ?? ""
+    let lastName: String  = UserDefaults.standard.string(forKey: kLastName) ?? ""
+    let email: String  = UserDefaults.standard.string(forKey: kEmail) ?? ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Personal Information")
+            Image("profile-image-placeholder")
+            Text(firstName)
+            Text(lastName)
+            Text(email)
+            Button("Logout"){
+                
+            }
+        }
     }
 }
 
